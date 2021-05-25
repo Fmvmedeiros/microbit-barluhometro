aac611c69057909e4b4bf2fefab69a4969dcdd4e
Fmvmedeiros <84106516+Fmvmedeiros@users.noreply.github.com>
2021-05-25 12:14:40 +0100

    Ajuste do tempo de Calculo

--- main.ts
+++ main.ts
@@ -1,16 +1,47 @@
 input.onButtonPressed(Button.A, function () {
-    music.playTone(698, music.beat(BeatFraction.Quarter))
-    ON = 1
-    T0 = control.millis()
-    sample_count = 0
+    if (ON == 0) {
+        music.playTone(698, music.beat(BeatFraction.Quarter))
+        ON = 1
+        T0 = control.millis()
+        sample_count = 0
+    } else if (ON == 2) {
+        TempoCalculo += -1000
+        if (TempoCalculo <= 1000) {
+            TempoCalculo = 1000
+        }
+        basic.showNumber(TempoCalculo / 1000)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (ON == 0) {
+        ON = 2
+        basic.showString("TEMPO")
+        basic.showNumber(TempoCalculo / 1000)
+    } else if (ON == 2) {
+        ON = 0
+        basic.showIcon(IconNames.EigthNote)
+    } else {
+    	
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(barulho / sample_count)
+    if (ON == 0) {
+        basic.showNumber(barulho / sample_count)
+    } else if (ON == 2) {
+        TempoCalculo += 1000
+        basic.showNumber(TempoCalculo / 1000)
+    } else {
+    	
+    }
 })
 let barulho = 0
 let sample_count = 0
 let T0 = 0
 let ON = 0
+let TempoCalculo = 0
+TempoCalculo = 7000
 let fundo = input.soundLevel()
 basic.showIcon(IconNames.EigthNote)
 basic.forever(function () {
@@ -21,7 +52,7 @@ basic.forever(function () {
         input.soundLevel(),
         255
         )
-        if (control.millis() - T0 > 7000) {
+        if (control.millis() - T0 > TempoCalculo) {
             ON = 0
             music.playTone(698, music.beat(BeatFraction.Quarter))
             basic.pause(50)
